f5e968feeb33e998df5f698e1e12cbf9c2b2b669
Lawe02 <89772680+Lawe02@users.noreply.github.com>
2025-10-02 13:02:14 +0200

    wip

---
+++ AspireLetter.AppHost/AppHost.cs
@@ -0,0 +1,7 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+// Add SQL Server container (named "identity-sql")
+var db = builder.AddSqlServer("identity-sql")
+                .AddDatabase("identitydb");
+
+builder.Build().Run();
