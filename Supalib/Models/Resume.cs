8c78d10b8093f9a5c29ade251f9660f65a3f17ce
Lawe02 <89772680+Lawe02@users.noreply.github.com>
2025-10-25 21:37:37 +0200

    removed comments from resume class

---
+++ Supalib/Models/Resume.cs
@@ -0,0 +1,10 @@
+namespace Supalib.Models
+{
+    public class Resume
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string Content { get; set; }
+        
+    }
+}
