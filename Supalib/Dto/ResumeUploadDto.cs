b15fb3ba4e7ef9f588ea66bb1db188c2c6858071
Lawe02 <89772680+Lawe02@users.noreply.github.com>
2025-10-30 21:34:14 +0100

    empty resumeuploaddto

---
+++ Supalib/Dto/ResumeUploadDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supalib.Dto
+{
+    public class ResumeUploadDto
+    {
+    }
+}
