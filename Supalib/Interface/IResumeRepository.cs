dd091ca2b31df77c61df7e836c675c7dd12bfc33
Lawe02 <89772680+Lawe02@users.noreply.github.com>
2025-10-30 23:53:06 +0100

    implementing interfaces

--- Supalib/Interface/IResumeRepository.cs
+++ Supalib/Interface/IResumeRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Supalib.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace Supalib.Interface
 {
     public interface IResumeRepository
     {
+        public Task SaveResumeAsync(Resume resume);
     }
 }
