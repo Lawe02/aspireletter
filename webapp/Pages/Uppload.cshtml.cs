4cf94aee59e284f8dc9645c619cf237583324fd4
Lawe02 <89772680+Lawe02@users.noreply.github.com>
2025-11-09 18:59:57 +0100

    uppload pagemodel

--- webapp/Pages/Uppload.cshtml.cs
+++ webapp/Pages/Uppload.cshtml.cs
@@ -1,12 +1,35 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Supalib.Dto;
+using Supalib.Interface;
+using Supalib.Models;
 
 namespace webapp.Pages
 {
     public class UpploadModel : PageModel
     {
+        private readonly IResumeService _resumeService;
+        private readonly UserManager<ApplicationUser> _userManager;
+        
+        public UpploadModel(IResumeService resumeService)
+        {
+            _resumeService = resumeService;
+        }
+
+        public ResumeUploadDto Resume { get; set; }
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            var userId = _userManager.GetUserId(User);
+
+            await _resumeService.UploadResumeAsync(Resume, userId);
+            return RedirectToPage("Success");
         }
     }
 }
