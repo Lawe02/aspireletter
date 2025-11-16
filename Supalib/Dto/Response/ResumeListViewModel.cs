4f2fd833816d2610348b42ca574626fd997ca120
Lawe02 <89772680+Lawe02@users.noreply.github.com>
2025-11-16 23:42:24 +0100

    resumelistviewmodel done

---
+++ Supalib/Dto/Response/ResumeListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supalib.Dto.Response
+{
+    public class ResumeListViewModel
+    {
+        public List<ResumeViewModel> Resumes { get; set; } = new List<ResumeViewModel>();
+    }
+}
