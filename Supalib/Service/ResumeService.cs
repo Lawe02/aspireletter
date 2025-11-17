6638ef088b65d744df78a6c6f1a5a367a825f1ee
Lawe02 <89772680+Lawe02@users.noreply.github.com>
2025-11-17 23:45:03 +0100

    adjusted spell error

--- Supalib/Service/ResumeService.cs
+++ Supalib/Service/ResumeService.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Supalib.Dto.Response;
 
 namespace Supalib.Service
 {
@@ -32,6 +33,27 @@ namespace Supalib.Service
             await dto.File.CopyToAsync(ms);
             resume.Data = ms.ToArray();
             await _repo.SaveResumeAsync(resume);
+        }
+
+        public async Task<ResumeListViewModel> GetResumesAsync(string userId)
+        {
+            List<Resume> resumes = await _repo.GetResumesAsync(userId);
+
+            ResumeListViewModel resumeListViewModel = new ResumeListViewModel
+            {
+                Resumes = resumes
+                .Select(resume => new ResumeViewModel
+                {
+                    Id = resume.Id,
+                    FileName = resume.FileName,
+                    ContentType = resume.ContentType,
+                    Size = resume.Size,
+                    UpploadedAt = resume.UploadedAt,
+                    FileUrl = $"/test/{resume.Id}"
+                }).ToList()
+            };
+
+            return resumeListViewModel;
         }
     }
 }
