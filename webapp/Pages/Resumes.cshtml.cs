f0e214fffc40f4bc7bda500e98fc4b386f2f8d7c
Lawe02 <89772680+Lawe02@users.noreply.github.com>
2025-11-20 22:32:16 +0100

    resumes pagemodel primarly done

--- webapp/Pages/Resumes.cshtml.cs
+++ webapp/Pages/Resumes.cshtml.cs
@@ -1,12 +1,27 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Supalib.Dto.Response;
+using Supalib.Interface;
+using Supalib.Models;
+using Supalib.Service;
 
 namespace webapp.Pages
 {
     public class ResumesModel : PageModel
     {
-        public void OnGet()
+        private readonly IResumeService _resumeService;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public ResumesModel(IResumeService resumeService, UserManager<ApplicationUser> userManager)
         {
+            _userManager = userManager;
+            _resumeService = resumeService;
+        }
+
+        public ResumeListViewModel Resumes { get; set; }
+        public async void OnGet()
+        {
+            Resumes = await _resumeService.GetResumesAsync(_userManager.GetUserId(User));
         }
     }
 }
